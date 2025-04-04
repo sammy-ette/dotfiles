693ed12ffba10239002961be8639efdc54912174
sammyette <torchedsammy@gmail.com>
2025-04-04 09:34:50 -0400

    fix: use default wallpaper if set wallpaper doesnt exist

--- ui/wallpaper.lua
+++ ui/wallpaper.lua
@@ -1,13 +1,22 @@
 local awful = require 'awful'
+local gears = require 'gears'
 local wibox = require 'wibox'
 local settings = require 'sys.settings'
 
 awful.screen.connect_for_each_screen(function(s)
+	local configWallpaper = settings.getConfig 'wallpaper'.home.image
+	if not gears.filesystem.file_readable(configWallpaper) then
+		configWallpaper = gears.filesystem.get_configuration_dir() .. '/assets/lotus-wallpaper.png'
+	end
+
 	awful.wallpaper {
 		screen = s,
 		widget = {
 			{
-				image = settings.getConfig 'wallpaper'.home.image,
+				image = gears.surface.crop_surface {
+					surface = gears.surface.load_uncached(configWallpaper),
+					ratio = s.geometry.width/s.geometry.height,
+				},
 				resize = true,
 				widget = wibox.widget.imagebox
 			},
