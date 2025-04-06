b165fffd79dffb45f7d59bfe1592b7c090f8f708
sammyette <torchedsammy@gmail.com>
2025-04-06 10:55:13 -0400

    fix: set default app icon

--- sys/client.lua
+++ sys/client.lua
@@ -1,5 +1,7 @@
 local awful = require 'awful'
+local gears = require 'gears'
 local ruled = require 'ruled'
+local extrautils = require 'libs.extrautils'()
 
 ruled.client.connect_signal('request::rules', function()
 	ruled.client.append_rule {
@@ -38,6 +40,17 @@ end
 client.connect_signal('manage', function(c)
 	restrictHeight(c)
 	if not awesome.startup then awful.client.setslave(c) end
+
+	local cairo = require('lgi').cairo
+	local default_icon = extrautils.apps.lookup_icon 'application-x-executable'
+	if c and c.valid and not c.icon then
+		local s = gears.surface(default_icon)
+		local img = cairo.ImageSurface.create(cairo.Format.ARGB32, s:get_width(), s:get_height())
+		local cr = cairo.Context(img)
+		cr:set_source_surface(s, 0, 0)
+		cr:paint()
+		c.icon = img._native
+	end
 end)
 
 client.connect_signal('request::geometry', function(c)
