8dc666f07e7dfc560fd747541fdbc2e83b09c8ba
sammyette <torchedsammy@gmail.com>
2025-04-15 23:01:15 -0400

    fix: make workspaces panel look a bit better, include client icons

--- ui/panels/workspaces.lua
+++ ui/panels/workspaces.lua
@@ -33,7 +33,9 @@ local workspaces = panels.create {
 			layout = workspacesList
 		}
 	},
-	attach = 'left'
+	attach = 'left',
+	growHeight = false,
+	growWidth = true
 }
 
 local function createWorkspaceWid(t)
@@ -44,16 +46,34 @@ local function createWorkspaceWid(t)
 
 	local height = util.dpi(120)
 
+	local appsGrid = wibox.widget {
+		layout = wibox.layout.grid,
+		column_count = 2,
+		orientation = 'horizontal',
+		--min_column_height = util.dpi(32),
+		homogeneous = true,
+		expand = true,
+		spacing = util.dpi(5)
+	}
+	for _, c in pairs(t:clients()) do
+		appsGrid:add(wibox.widget {
+			layout = wibox.container.constraint,
+			width = util.dpi(32),
+			awful.widget.clienticon(c)
+		})
+	end
+
 	return wibox.widget {
 		widget = wibox.container.constraint,
 		height = height,
 		width = util.dpi(240),
+		strategy = 'exact',
 		{
 			widget = wibox.container.background,
 			bg = '#0000000',
 			shape = util.rrect(beautiful.radius),
 			border_width = util.dpi(2),
-			border_color = beautiful.foreground,
+			border_color = beautiful.shade2,
 			{
 				layout = wibox.layout.stack,
 				{
@@ -66,7 +86,11 @@ local function createWorkspaceWid(t)
 				},
 				{
 					widget = wibox.container.background,
-					bg = beautiful.panelBackground .. string.format('%x', math.floor(0.6 * 255))
+					bg = beautiful.shade2 .. string.format('%x', math.floor(0.8 * 255))
+				},
+				{
+					layout = wibox.container.place,
+					appsGrid
 				}
 			}
 		}
@@ -76,10 +100,12 @@ end
 local oldOn = workspaces.on
 function workspaces:on(...)
 	local scr = awful.screen.focused()
+	workspacesList:reset()
+	setupWorkspaceList()
 	for _, t in ipairs(scr.tags) do
 		local clients = t:clients()
 		print(#clients, t.index)
-		if #clients == 0 then
+		if #clients ~= 0 then
 			print 'adding workspace'
 			local workspaceWid = createWorkspaceWid(t)
 			workspacesList:add(workspaceWid)
