8a3afa465a419191709adfce34a9d18fa3540c4b
sammyette <torchedsammy@gmail.com>
2025-04-16 21:15:00 -0400

    fix: set wallpaper via request::wallpaper signal

--- ui/wallpaper.lua
+++ ui/wallpaper.lua
@@ -3,18 +3,13 @@ local gears = require 'gears'
 local wibox = require 'wibox'
 local settings = require 'sys.settings'
 
-awful.screen.connect_for_each_screen(function(s)
-	local configWallpaper = settings.getConfig 'wallpaper'.home.image
-	if not gears.filesystem.file_readable(configWallpaper) then
-		configWallpaper = gears.filesystem.get_configuration_dir() .. '/assets/lotus-wallpaper.png'
-	end
-
+screen.connect_signal('request::wallpaper', function(s)
 	awful.wallpaper {
 		screen = s,
 		widget = {
 			{
 				image = gears.surface.crop_surface {
-					surface = gears.surface.load_uncached(configWallpaper),
+					surface = gears.surface.load_uncached(settings.getConfig 'wallpaper'.home.image, gears.filesystem.get_configuration_dir() .. '/assets/lotus-wallpaper.png'),
 					ratio = s.geometry.width/s.geometry.height,
 				},
 				resize = true,
